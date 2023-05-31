f4a688187f1a02196e262323323be8916c7cc72f
thmnguyen <78154426+thmnguyen@users.noreply.github.com>
2023-05-31 10:18:27 +1000

    Update analytics.php

--- analytics.php
+++ analytics.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="Admin Panel">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/style_manage.css"> 
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
